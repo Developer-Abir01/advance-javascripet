03fe91e08df9d1d98e96a55d866184ac5f001cd4
Abirk2day <abirk2day@gmail.com>
2021-02-02 20:43:51 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// 0
+// ""
+// undefined
+// null
+// Truthy
+
+// '0', ' ' []
+
+// const name = 'salamn khan';
+let name = 12;
+
+
+if( name || name == 0){
+  console.log("condisonl id true");
+}
+else{
+    console.log("condisonal is false");
+}
