564bd2f300c70b0a8022609e14e048d3c5d5d077
Abirk2day <abirk2day@gmail.com>
2021-02-05 19:01:33 +0600

    sceop

---
+++ scope.js
@@ -0,0 +1,16 @@
+let bonus = 20;
+
+function sum (first, second){
+    let result = first + second + bonus;
+    // console.log(result);
+    if(result > 9){
+        let mood = "happy";
+        mood = "fisky"
+        console.log(mood);
+    }
+    console.log(mood);
+    return result
+}
+const output = sum(3, 7);
+console.log(bonus);
+console.log(output);
